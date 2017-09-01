5a5c0d8ead20a5e6ee547301b66dabf5eb531e75
Thaer Khawaja <thaer.khawaja@gmail.com>
2017-08-31 21:50:45 -0700

    Temp remove seed dns entries.

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -130,12 +130,8 @@ namespace nodetool
     virtual bool unblock_host(const epee::net_utils::network_address &address);
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
-    const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.masariseeds.se"
-    , "seeds.masariseeds.ae.org"
-    , "seeds.masariseeds.ch"
-    , "seeds.masariseeds.li"
-    };
+    // TODO-TK: need to configure dns entries.
+    const std::vector<std::string> m_seed_nodes_list = {};
 
     bool islimitup=false;
     bool islimitdown=false;
