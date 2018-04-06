fb96adb5e377f1ba89d6d8d2805819815e74cbe6
Thaer Khawaja <thaer.khawaja@gmail.com>
2018-04-05 22:28:46 -0700

    Missing change from PR.

--- tests/unit_tests/address_from_url.cpp
+++ tests/unit_tests/address_from_url.cpp
@@ -84,7 +84,7 @@ TEST(AddressFromTXT, Failure)
 
 TEST(AddressFromURL, Success)
 {
-  const std::string addr = MONERO_DONATION_ADDR;
+  const std::string addr = MASARI_DONATION_ADDR;
   
   bool dnssec_result = false;
 
