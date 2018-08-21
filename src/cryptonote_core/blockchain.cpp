37199f03f846be6bfa16969ea02e9e0754494b95
cryptochangements34 <bW9uZXJv@gmail.com>
2018-08-20 20:29:31 -0400

    allow nephew miner to claim 5% bonus reward

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1106,12 +1106,28 @@ bool Blockchain::validate_miner_transaction(const block& b, size_t cumulative_bl
     MERROR_VER("block size " << cumulative_block_size << " is bigger than allowed for this blockchain");
     return false;
   }
-  if(base_reward + fee < money_in_use)
+  if(m_hardfork->get_current_version() > 7)
   {
-    MERROR_VER("coinbase transaction spend too much money (" << print_money(money_in_use) << "). Block reward is " << print_money(base_reward + fee) << "(" << print_money(base_reward) << "+" << print_money(fee) << ")");
-    return false;
+    if(is_uncle_block_included(b))
+    {
+      uint64_t max_nephew_reward = base_reward / 20;
+      if(base_reward  + max_nephew_reward + fee < money_in_use)
+      {
+        MERROR_VER("coinbase transaction spend too much money (" << print_money(money_in_use) << "). Block reward is " << print_money(base_reward + max_nephew_reward + fee) << "(" << print_money(base_reward) << "+" << print_money(max_nephew_reward) << "+" << print_money(fee) << ")");
+        return false;
+      }
+    }
+    else
+    {
+      if(base_reward + fee != money_in_use)
+      {
+        MERROR_VER("coinbase transaction spend too much money or does not use full block reward amount (" << print_money(money_in_use) << "). Block reward is " << print_money(base_reward + fee) << "(" << print_money(base_reward) << "+" << print_money(fee) << ")");
+        return false;
+      }
+    }
   }
-  if (m_hardfork->get_current_version() >= 1)
+  
+  if (m_hardfork->get_current_version() >= 1 && m_hardfork->get_current_version() < 8)
   {
     if(base_reward + fee != money_in_use)
     {
