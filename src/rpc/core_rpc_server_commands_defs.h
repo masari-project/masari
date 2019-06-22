535a0e86d498304e5799ebb7fe1998f135f1bae2
cryptochangements34 <bW9uZXJv@gmail.com>
2019-06-21 18:36:09 -0700

    remove outdated compatability variable initialization from gettransactions_by_heights

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -597,10 +597,6 @@ namespace cryptonote
 
     struct response
     {
-      // older compatibility stuff
-      std::list<std::string> txs_as_hex;  //transactions blobs as hex (old compat)
-      std::list<std::string> txs_as_json; //transactions decoded as json (old compat)
-
       // in both old and new
       std::list<std::string> missed_tx;   //not found transactions
 
@@ -610,8 +606,6 @@ namespace cryptonote
       bool untrusted;
 
       BEGIN_KV_SERIALIZE_MAP()
-        KV_SERIALIZE(txs_as_hex)
-        KV_SERIALIZE(txs_as_json)
         KV_SERIALIZE(txs)
         KV_SERIALIZE(missed_tx)
         KV_SERIALIZE(status)
