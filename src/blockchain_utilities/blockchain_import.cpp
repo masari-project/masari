fc3f5188be5de3fcc98ec14a03c44d59afd405fc
ArqTras <biuro@ar-net.info>
2019-12-27 22:53:41 +0100

    typo

--- src/blockchain_utilities/blockchain_import.cpp
+++ src/blockchain_utilities/blockchain_import.cpp
@@ -644,7 +644,7 @@ int main(int argc, char* argv[])
 
   if (command_line::get_arg(vm, command_line::arg_help))
   {
-    std::cout << "ArQmA '" << ARQMA_RELEASE_NAME << "' (v" << ARQMA_VERSION_FULL << ")" << ENDL << ENDL;
+    std::cout << "masari '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
     std::cout << desc_options << std::endl;
     return 1;
   }
@@ -682,7 +682,7 @@ int main(int argc, char* argv[])
   m_config_folder = command_line::get_arg(vm, cryptonote::arg_data_dir);
   db_arg_str = command_line::get_arg(vm, arg_database);
 
-  mlog_configure(mlog_get_default_log_path("arqma-blockchain-import.log"), true);
+  mlog_configure(mlog_get_default_log_path("masari-blockchain-import.log"), true);
   if (!command_line::is_arg_defaulted(vm, arg_log_level))
     mlog_set_log(command_line::get_arg(vm, arg_log_level).c_str());
   else
