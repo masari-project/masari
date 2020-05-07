de0f008747c7ae66e3ded328eb1a79053f00c690
camthegeek <cam@camthegeek.net>
2020-05-07 18:16:30 -0400

    it's masarid, not monerod.

--- src/daemon/rpc_command_executor.cpp
+++ src/daemon/rpc_command_executor.cpp
@@ -1200,10 +1200,10 @@ bool t_rpc_command_executor::print_status()
   bool daemon_is_alive = m_rpc_client->check_connection();
 
   if(daemon_is_alive) {
-    tools::success_msg_writer() << "monerod is running";
+    tools::success_msg_writer() << "masarid is running";
   }
   else {
-    tools::fail_msg_writer() << "monerod is NOT running";
+    tools::fail_msg_writer() << "masarid is NOT running";
   }
 
   return true;
